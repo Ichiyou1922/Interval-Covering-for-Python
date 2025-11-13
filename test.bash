b78a4386d81c058b265656200d5f7cefe5ab6880
Ichiyou1922 <mogi2fruits.kazu@gmail.com>
2025-11-13 20:52:57 +0900

    change test.bash

--- test.bash
+++ test.bash
@@ -15,20 +15,17 @@ out=$(seq 5 | ./incov)
 [ "$res" = 0 ] && echo OK
 
 testdata1='4\n7\n9\n5\n1\n10\n13\n5\n6'
-
-out=$(printf "$testdata1" |./incov)
+out=$(printf "%b" "$testdata1" |./incov)
 [ "$out" = 5 ] || ng "$LINENO"
 [ "$res" = 0 ] && echo OK
 
 testdata2='-4\n-2\n-8\n-6\n0\n1\n4\n7\n3'
-
-out=$(printf "$testdata2" | ./incov)
+out=$(printf "%b" "$testdata2" | ./incov)
 [ "$out" = 5 ] || ng "$LINENO"
 [ "$res" = 0 ] && echo OK
 
 testdata3='3\n2\n5\n5.6\n6\n0\n1.53\n2\n5\n5.6\n6\n0\n1.5'
-
-out=$(printf "$testdata3" | ./incov)
+out=$(printf "%b" "$testdata3" | ./incov)
 [ "$out" = 3 ] || ng "$LINENO"
 [ "$res" = 0 ] && echo OK
 
