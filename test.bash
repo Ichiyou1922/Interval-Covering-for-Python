2900d9216695e774b7203839c2906b7ca05458d0
Ichiyou1922 <mogi2fruits.kazu@gmail.com>
2025-11-13 20:31:47 +0900

    change test.bash

--- test.bash
+++ test.bash
@@ -14,17 +14,17 @@ out=$(seq 5 | ./incov)
 [ "$out" = 2 ] || ng "$LINENO"
 [ "$res" = 0 ] && echo OK
 
-testdata1=$(print "4\n7\n9\n5\n1\n10\n13\n5\n6")
-out=$(echo "$testdata1" |./incov) 
+testdata1=$(printf "4\n7\n9\n5\n1\n10\n13\n5\n6")
+out=$("$testdata1" |./incov) 
 [ "$out" = 5 ] || ng "$LINENO"
 [ "$res" = 0 ] && echo OK
 
-testdata2=$(print "-4\n-2\n-8\n-6\n0\n1\n4\n7\n3")
+testdata2=$(printf "-4\n-2\n-8\n-6\n0\n1\n4\n7\n3")
 out=$(echo "$testdata2" | ./incov)
 [ "$out" = 5 ] || ng "$LINENO"
 [ "$res" = 0 ] && echo OK
 
-testdata3=$(print "3\n2\n5\n5.6\n6\n0\n1.53\n2\n5\n5.6\n6\n0\n1.5")
+testdata3=$(printf "3\n2\n5\n5.6\n6\n0\n1.53\n2\n5\n5.6\n6\n0\n1.5")
 out=$(echo "$testdata3" | ./incov)
 [ "$out" = 3 ] || ng "$LINENO"
 [ "$res" = 0 ] && echo OK
