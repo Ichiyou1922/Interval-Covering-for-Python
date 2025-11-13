f5353c9a8f6683ccd9dc8480b98c41145deb27c7
Ichiyou1922 <mogi2fruits.kazu@gmail.com>
2025-11-13 20:49:04 +0900

    change test.bash

--- test.bash
+++ test.bash
@@ -3,8 +3,8 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo ${1}行目が間違っています
-	res=1
+        echo ${1}行目が間違っています
+        res=1
 }
 
 res=0
@@ -15,17 +15,20 @@ out=$(seq 5 | ./incov)
 [ "$res" = 0 ] && echo OK
 
 testdata1='4\n7\n9\n5\n1\n10\n13\n5\n6'
-out=$(echo testdata1 |./incov) 
+
+out=$(printf "$testdata1" |./incov)
 [ "$out" = 5 ] || ng "$LINENO"
 [ "$res" = 0 ] && echo OK
 
 testdata2='-4\n-2\n-8\n-6\n0\n1\n4\n7\n3'
-out=$(echo testdata2 | ./incov)
+
+out=$(printf "$testdata2" | ./incov)
 [ "$out" = 5 ] || ng "$LINENO"
 [ "$res" = 0 ] && echo OK
 
 testdata3='3\n2\n5\n5.6\n6\n0\n1.53\n2\n5\n5.6\n6\n0\n1.5'
-out=$(echo testdata3 | ./incov)
+
+out=$(printf "$testdata3" | ./incov)
 [ "$out" = 3 ] || ng "$LINENO"
 [ "$res" = 0 ] && echo OK
 
