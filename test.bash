8384b43d2445f16b8c12423819e11cd2c91a30af
Ichiyou1922 <mogi2fruits.kazu@gmail.com>
2025-11-13 17:15:05 +0900

    add test data

---
+++ test.bash
@@ -0,0 +1,15 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Mogi Kazuha <mogi2fruits.kazu@gmail.com>
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo ${1}行目が間違っています
+	res=1
+}
+
+res=0
+
+### Normal Input ###
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Mogi Kazuha <mogi2fruits.kazu@gmail.com>
+# SPDX-License-Identifier: BSD-3-Clause
